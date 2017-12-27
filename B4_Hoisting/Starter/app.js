56fb76e5f5cdf7ff501001b267d98f25691529c0
Gage Wagner <gageplwagner@gmail.com>
2017-12-27 15:35:29 -0600

    hoisting

--- B4_Hoisting/Starter/app.js
+++ B4_Hoisting/Starter/app.js
@@ -1,5 +1,20 @@
 var a = 'Hello World!';
 
 function b() {
-    
+    console.log('Called b!');
 }
+
+b();
+console.log(a);
+
+/*
+    Execution Content is created in two different phases:
+
+    #1: CREATION PHASE
+        - Global Object, 'this', Outer Environment
+        - Setup Memory Space for Variables and Functions
+        - a.k.a. "Hoisting"
+
+    #2: EXECUTION PHASE 
+
+*/
