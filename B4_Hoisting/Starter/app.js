1a8b9d086f1d00dcfc749347b981d5e9e2708ef6
Gage Wagner <gageplwagner@gmail.com>
2017-12-27 15:51:16 -0600

    undefined

--- B4_Hoisting/Starter/app.js
+++ B4_Hoisting/Starter/app.js
@@ -1,11 +1,21 @@
-var a = 'Hello World!';
+// var a = 'Hello World!';
 
-function b() {
-    console.log('Called b!');
+// function b() {
+//     console.log('Called b!');
+// }
+
+// b();
+// console.log(a);
+
+var a;
+console.log(a);
+
+if (a === undefined) {
+    console.log('a is undefined!');
+} else {
+    console.log('a is defined!');
 }
 
-b();
-console.log(a);
 
 /*
     Execution Content is created in two different phases:
@@ -14,6 +24,8 @@ console.log(a);
         - Global Object, 'this', Outer Environment
         - Setup Memory Space for Variables and Functions
         - a.k.a. "Hoisting"
+
+        Variables are set equal to 'undefined' and Functions Setup.
 
     #2: EXECUTION PHASE 
 
